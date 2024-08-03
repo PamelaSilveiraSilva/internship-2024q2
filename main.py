a1c7f14345dd5b9fb2ac6e06f9a498f46d67f2e1
PamelaSilveiraSilva <169115187+PamelaSilveiraSilva@users.noreply.github.com>
2024-08-02 22:46:38 -0300

    v3
    v3

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ from solution.solution import SelicCalc
 from datetime import date
 
 if __name__ == '__main__':
+    # Instancia a classe SelicCalc
     calc = SelicCalc()
 
+    # Calcula e obtém o DataFrame com os resultados
     df = calc.calc_amount(
         start_date=date(2010, 1, 1),
         end_date=date(2021, 3, 1),
@@ -11,5 +13,3 @@ if __name__ == '__main__':
         frequency="daily",
         save_csv=False,
     )
-
-    print(df)
