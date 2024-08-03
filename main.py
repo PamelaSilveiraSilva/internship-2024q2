7bbbfd55bf8ab074c4ecdf3e52f5d41a700296f4
PamelaSilveiraSilva <169115187+PamelaSilveiraSilva@users.noreply.github.com>
2024-08-02 22:40:04 -0300

    v2
    v2

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 from solution.solution import SelicCalc
-
 from datetime import date
-
 
 if __name__ == '__main__':
     calc = SelicCalc()
 
-    calc.calc_amount_earned(
+    df = calc.calc_amount(
         start_date=date(2010, 1, 1),
         end_date=date(2021, 3, 1),
         capital=657.43,
         frequency="daily",
         save_csv=False,
     )
+
+    print(df)
